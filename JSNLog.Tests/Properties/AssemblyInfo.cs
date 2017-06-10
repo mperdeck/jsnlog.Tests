49e3cb92e6c1a97f585f1d02d4fc4aa93926ec15
mperdeck <matt.perdeck@readify.net>
2017-06-10 17:42:23 +1000

    fix up project reference

--- JSNLog.Tests/Properties/AssemblyInfo.cs
+++ JSNLog.Tests/Properties/AssemblyInfo.cs
@@ -1,29 +1,20 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JSNLog.Tests")]
+[assembly: AssemblyTitle("jsnlog.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JSNLog.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("jsnlog.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("650f9c8c-3496-416b-9e73-29b74b074a45")]
+[assembly: Guid("9b07b5e0-3b3e-41ce-9c72-df3740b49bfd")]
 
-// Get the compiler to create a strong named assembly, using the key pair in this file.
-// This way, this assembly can be a friend of JSNLog, which is a strong named assembly itself.
-[assembly: AssemblyKeyFileAttribute("../jsnlog.strongname.snk")]
-
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
